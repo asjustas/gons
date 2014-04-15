190427985776d476abb7491e941cbc185b34d29f
Justas V <justas@u3.lt>
2014-04-15 23:12:25 +0300

    Added ini config file

--- gons.go
+++ gons.go
@@ -8,6 +8,12 @@ import (
 )
 
 func main() {
+	conf, err := goini.Load("config.ini")
+
+	if err != nil {
+        panic(err)
+    }
+
 	m := martini.Classic()
 	log.Fatal(http.ListenAndServe(":8080", m))
 	runtime.GOMAXPROCS(runtime.NumCPU())
